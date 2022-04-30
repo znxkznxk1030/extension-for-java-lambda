6bb06ca9ac784bc477ec6d09cc3c998fc08c5151
ysKim <znxkznxk1030@gmail.com>
2022-04-30 19:36:58 +0530

    [ADD] absctract class Wizard

--- src/wizard/wizard.ts
+++ src/wizard/wizard.ts
@@ -1,8 +1,20 @@
+import { Prompter } from "../ui/prompter";
 
-export class wizard {
-  
-}
+export class wizard {}
 
-export abstract class Wizard {
+export abstract class Wizard<TResult> {
+  private readonly response: Partial<TResult> = {};
+  private readonly prompterList: Prompter<any>[] = [];
 
+  protected constructor() {}
+
+  public async run(): Promise<TResult | undefined> {
+    for (const prompter of this.prompterList) {
+      const ret = await prompter.interact();
+    }
+
+    return this.getResult();
+  }
+
+  protected abstract getResult(): TResult | undefined;
 }
