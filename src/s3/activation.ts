f4e19f08d8e6f528e9cb5841f93569c8e8ee7598
ysKim <znxkznxk1030@gmail.com>
2022-05-23 17:48:05 +0200

    [ADD] createObject | find files **/*.zip|jar

--- src/s3/activation.ts
+++ src/s3/activation.ts
@@ -1,5 +1,7 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
 import { S3BucketExplorer } from "../s3/explorer/S3BucketExplorer";
+import glob = require("glob");
 
 export async function activateS3(context: vscode.ExtensionContext) {
   const s3BucketTreeProvider = new S3BucketExplorer();
@@ -17,8 +19,11 @@ export async function activateS3(context: vscode.ExtensionContext) {
   );
 
   context.subscriptions.push(
-    vscode.commands.registerCommand("s3buckets.createObject", () => {
+    vscode.commands.registerCommand("s3buckets.createObject", async () => {
       console.log("=== s3buckets.createObject ===");
+      const files = await vscode.workspace.findFiles("**/*.zip", "**/*.jar");
+
+      console.log(files);
     })
   );
 }
