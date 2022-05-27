630327a7eab9739936253d30c01e69f8197dccd5
ysKim <znxkznxk1030@gmail.com>
2022-05-28 00:42:39 +0900

    [ADD] bucketPrompter at uploadObjectWizard

--- src/s3/commands/uploadObject.ts
+++ src/s3/commands/uploadObject.ts
@@ -5,13 +5,15 @@ import {
   UploadObejctWizard,
 } from "../wizard/UploadObjectWizard";
 import { DefaultPickItem, PickPrompter } from "../../ui/prompter";
+import { s3Client } from "../../clients/s3Client";
+import { Bucket, ListBucketsCommand } from "@aws-sdk/client-s3";
 
 export async function uploadObject() {
   const filePrompter = new PickPrompter({
     id: "s3object",
-    title: "(1/2) Select a file to upload",
+    title: "(2/3) Select a file to upload",
     loadItemsAsync: async (context: TUploadObjectWizardContext) => {
-      const files:vscode.Uri[] = await vscode.workspace.findFiles("**/*.jar");
+      const files: vscode.Uri[] = await vscode.workspace.findFiles("**/*.jar");
 
       return files;
     },
@@ -25,7 +27,7 @@ export async function uploadObject() {
       _resolve: (value: PromiseLike<undefined> | undefined) => void,
       reject: (reason?: any) => void
     ) => {
-      const file:vscode.Uri = item.data as vscode.Uri;
+      const file: vscode.Uri = item.data as vscode.Uri;
 
       if (!!!file || /^\s*$/.test(file.path)) {
         reject("[Error] A file is mandatory.");
@@ -33,9 +35,36 @@ export async function uploadObject() {
     },
   });
 
+  const bucketPrompter = new PickPrompter({
+    id: "bucket",
+    title: "(3/3) Select S3 Bucket",
+    loadItemsAsync: async (context: TUploadObjectWizardContext) => {
+      let { Buckets: buckets } = await s3Client.send(
+        new ListBucketsCommand({})
+      );
+
+      return buckets;
+    },
+    mapperToPickItem: (bucketList?: Bucket[]) => {
+      return bucketList?.map((bucket) => {
+        return new DefaultPickItem(bucket.Name, bucket);
+      });
+    },
+    verifyPickItem: (
+      item: any,
+      _resolve: (value: PromiseLike<undefined> | undefined) => void,
+      reject: (reason?: any) => void
+    ) => {
+      if (!!!item || /^\s*$/.test(item)) {
+        reject("[Error] A S3 Bucket is mandatory.");
+      }
+    },
+  });
+
   const wizard = new UploadObejctWizard({});
 
   wizard.addPrompter(filePrompter);
+  wizard.addPrompter(bucketPrompter);
 
   let payload = await wizard.run();
 
